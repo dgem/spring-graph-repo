ab2ae1d08ce9f9a0e44cd545a1b3459a4dac8c03
Murphy <dan.murphy@ft.com>
2015-11-30 17:37:54 +0000

    Remove code from run() method in Neo4jRepositoryApplication

--- src/main/java/com/ft/spikes/Neo4jRepositoryApplication.java
+++ src/main/java/com/ft/spikes/Neo4jRepositoryApplication.java
@@ -21,31 +21,6 @@ public class Neo4jRepositoryApplication implements CommandLineRunner {
 
     public void run(String... args) throws Exception {
 
-//        Person greg = new Person("Greg");
-//        Person roy = new Person("Roy");
-//        Person craig = new Person("Craig");
-//
-//        System.out.println("Before linking up with Neo4j...");
-//        for (Person person : new Person[] { greg, roy, craig }) {
-//            System.out.println(person);
-//        }
-//
-//            personRepository.save(greg);
-//            personRepository.save(roy);
-//            personRepository.save(craig);
-//
-//            greg = personRepository.findByName(greg.name);
-//
-//            System.out.println("Lookup each person by name...");
-//            for (String name : new String[] { greg.name, roy.name, craig.name }) {
-//                System.out.println(personRepository.findByName(name));
-//            }
-//
-////            System.out.println("Looking up who works with Greg...");
-////            for (Person person : personRepository.findByTeammatesName("Greg")) {
-////                System.out.println(person.name + " works with Greg.");
-////            }
-
     }
 
     public static void main(String[] args) throws Exception {
